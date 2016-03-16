5a1629ff33414cba236f0b8ce7ce2b3909358d48
Corey Yaspan <cspan00@gmail.com>
2016-03-16 09:59:09 -0600

    showing posts in profile

--- public/js/controllers/profileController.js
+++ public/js/controllers/profileController.js
@@ -3,14 +3,9 @@ app.controller("profileController", function($scope, $http, posts, $location){
   console.log("profileController");
   posts.getUserData().then(function(result){
     $scope.user = result;
+    posts.getPostById(result.facebook_id).then(function(result){
+      $scope.posts = result;
+    })
 
   })
-
-
-  posts.getPostById("10153509558071342").then(function(result){
-    console.log(result);
-  })
-
-
-
 })
