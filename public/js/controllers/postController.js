b4394a03526b233a7d18d5b549feb3dfcf12993b
Scott Christensen <scottmc526@gmail.com>
2016-03-16 20:22:19 -0600

    admin button shows only when is_admin = true

--- public/js/controllers/postController.js
+++ public/js/controllers/postController.js
@@ -3,4 +3,18 @@ app.controller("postController", function($scope, $http, $auth, posts, $location
   posts.getPosts().then(function(response){
     $scope.posts = response;
   })
+
+  $scope.userData;
+  $scope.isAdmin;
+  posts.getUserData().then(function(payload){
+    $scope.userData = payload;
+    console.log(payload);
+    console.log($scope.userData['is_admin']);
+    if ($scope.userData['is_admin'] === true) {
+    $scope.isAdmin = true;
+  } else {
+    $scope.isAdmin = false;
+  }
+  console.log($scope.isAdmin);  
+  })
 })
