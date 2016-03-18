552cdb1eb34c16ec983ede3fca19376663044d6b
Scott Christensen <scottmc526@gmail.com>
2016-03-17 18:08:35 -0600

    show and hide edit button and delete button by user

--- public/js/controllers/postController.js
+++ public/js/controllers/postController.js
@@ -2,9 +2,16 @@
 app.controller("postController", function($scope, $http, $auth, posts, $location, $route){
   posts.getPosts().then(function(response){
     $scope.posts = response;
+    console.log("this is the response we want");
+    console.log(response);
+
   })
 
-  
+  posts.getUserData().then(function (user) {
+    $scope.user = user.facebook_id
+    console.log($scope.user);
+  })
+
 
 
   //checks to see if logged in user has is_admin set to true or false, the sets isAdmin to that value
