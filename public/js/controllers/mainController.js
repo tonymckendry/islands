02ee72d18ef4a48d9f2b21f5ce13787d58247cd3
TonyMcKendry <TonyMcKendry@gmail.com>
2016-04-05 09:51:07 -0600

    dictionary added and working, tweet checking algorithm base is started

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -4,10 +4,16 @@ app.controller('mainController', function($scope, $rootScope, $location, socket,
   $scope.streaming = true;
   // $scope.tweets = thing;
   socket.on('newTweet', function(tweet){
-    console.log($scope.streaming);
-      // console.log(tweet.text)
+    // console.log($scope.streaming);
       var obj = {}
       obj.tweet = tweet.text
+      var arr = tweet.text.split(' ')
+      for (var i = 0; i < arr.length; i++) {
+        if(dictionary[arr[i]]){
+          console.log("'" + arr[i] +  "' exists in dictionary")
+          console.log('sentiment: ' + dictionary[arr[i]])
+        }
+      }
       $scope.tweets.unshift(obj)
       // console.log($scope.tweets.length)
       if ($scope.streaming == false){
@@ -42,6 +48,7 @@ app.controller('mainController', function($scope, $rootScope, $location, socket,
     })
   }
 
+  var tester = 'OK,Trump is a globalist who wants to solve our trade problems with a massive tax hike on American consumers. @Jrgreen737 #Genius'
 
 
  });
