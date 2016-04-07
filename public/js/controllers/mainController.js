8acd3dcc8df88c4ab1dd07fc71c9b4d8ea9ce069
TonyMcKendry <TonyMcKendry@gmail.com>
2016-04-07 11:56:35 -0600

    trying to deploy

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,38 +1,97 @@
 app.controller('mainController', function($scope, $rootScope, $location, socket, $http) {
-  var thing = ["asef" , "sadsf", "asdfads"];
   $scope.tweets = []
   $scope.streaming = true;
   // $scope.tweets = thing;
   socket.on('newTweet', function(tweet){
-    // console.log($scope.streaming);
       var obj = {}
       obj.tweet = tweet.text
       var arr = tweet.text.split(' ')
+      var score = 0
+      var count = 0
       for (var i = 0; i < arr.length; i++) {
         if(dictionary[arr[i]]){
           console.log("'" + arr[i] +  "' exists in dictionary")
           console.log('sentiment: ' + dictionary[arr[i]])
+          score += dictionary[arr[i]]
+          count ++
         }
       }
+      console.log('Score is: ' + score);
+      console.log('Count is: ' + count);
+      var newNumber1 = score
+      var newNumber2 = count
+      if (count > 0) {
+        dataset.push([newNumber1, newNumber2])
+      }
       $scope.tweets.unshift(obj)
-      // console.log($scope.tweets.length)
       if ($scope.streaming == false){
         console.log('IN THE IF');
         $http.get('/stop').success(function(date){
 
         })
       }
+      /////////
+      var xScale = d3.scale.linear()
+      .domain([d3.min(dataset, function(d){return d[0]}), d3.max(dataset, function(d){return d[0]})])
+      // .domain([-500, 500])
+      .range([padding, w - padding])
+
+      var yScale = d3.scale.linear()
+      .domain([d3.min(dataset, function(d){return d[1]}), d3.max(dataset, function(d){return d[1]})])
+      // .domain([-100, 100])
+      .range([h - padding, padding])
+
+      var rScale = d3.scale.linear() // sets the scale for the dot radius
+      .domain([0, d3.max(dataset, function(d){ return d[1]})]) // finds the greatest Y value
+      .range([2, 7])
+      //////
+      svg.selectAll('circle') // creates the dots
+        .data(dataset)
+        .enter()
+        .append('circle')
+        .attr('cx', function(d){ //x position, scaled
+          return xScale(d[0])
+        })
+        .attr('cy', function(d){ //y position, scaled
+          return yScale(d[1])
+        })
+        .attr('r', function(d){ //dot radius, scaled
+          return rScale(d[1])
+        })
+        .attr('fill', function(d){
+          var r = Math.round(Math.random() * 255)
+          var g = 0
+          var b = 0
+          var color = 'rgb(' + r + ',' + g + ',' + b + ')'
+          return color
+        })
+      svg.selectAll('text') //labels the dpts
+        .data(dataset)
+        .enter()
+        .append('text')
+        .text(function(d){
+          return d[0] + ',' + d[1]
+        })
+        .attr('x', function(d){
+          return xScale(d[0])
+        })
+        .attr('y', function(d){
+          return yScale(d[1])
+        })
+        .attr('font-family', 'sans-serif')
+        .attr('font-size', '11px')
+        .attr('fill', 'black')
       $scope.$digest()
   })
 
-  $scope.callIt = function(term){
+  $scope.callIt = function(term){ //starts the tweet stream when you press the button
     console.log('CALLED IT');
     $http.get('/' + term).success(function(data){
     })
     $scope.streaming = true;
   }
 
-  $scope.stopIt = function(){
+  $scope.stopIt = function(){ //stops the tweet stream when you press the button
     console.log('Stop Button');
     $scope.streaming = false
     $http.get('/stop').success(function(data){
@@ -41,14 +100,12 @@ app.controller('mainController', function($scope, $rootScope, $location, socket,
     })
   }
 
-  $scope.restart=function(){
+  $scope.restart=function(){ //restarts the tweet stream when you press the button
     $scope.streaming=true;
     $http.get('/restart').success(function(data){
 
     })
   }
-
-  var tester = 'OK,Trump is a globalist who wants to solve our trade problems with a massive tax hike on American consumers. @Jrgreen737 #Genius'
 
 
  });
